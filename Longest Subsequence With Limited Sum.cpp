6e9d62ea675b5a32e0330d1a0adde70b7673cdff
Sumeet Kumar <64634052+sumeetkvd@users.noreply.github.com>
2022-12-25 23:16:13 +0530

    Create Longest Subsequence With Limited Sum.cpp

---
+++ Longest Subsequence With Limited Sum.cpp
@@ -0,0 +1,15 @@
+
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        sort(begin(nums), end(nums));
+        for (int i = 0; i + 1 < size(nums); ++i) {
+            nums[i + 1] += nums[i];
+        }
+        vector<int> result;
+        for (const auto& q : queries) {
+            result.emplace_back(distance(cbegin(nums), upper_bound(cbegin(nums), cend(nums), q)));
+        }
+        return result;
+    }
+};
